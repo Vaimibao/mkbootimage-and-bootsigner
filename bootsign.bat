33bb80c662e85681bd4f0c82135cd894848e009f
Xiyan <Zilchlan@yahoo.com>
2017-06-18 14:26:13 +0800

    fix sign boot
    When click the unpack-boot.bat,you will see "boot-new.img",click the bootsign.bat,actually,tool signes the boot.img,not boot-new.img

--- bootsign.bat
+++ bootsign.bat
@@ -1,3 +1,3 @@
 
-java -Xmx512M -jar BootSignature.jar boot boot.img ./security/verity.pk8 ./security/verity.x509.pem boot-new.img
+java -Xmx512M -jar BootSignature.jar boot boot-new.img ./security/verity.pk8 ./security/verity.x509.pem boot-new2.img
 
